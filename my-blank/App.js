057d75a716b221b57c88fca74ee2128e17ff1f64
Mario_Brnal10 <145825327+MarioBrnal10@users.noreply.github.com>
2025-06-11 18:31:04 -0600

    uso de propiedad children

--- my-blank/App.js
+++ my-blank/App.js
@@ -3,9 +3,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const Texto=(props)=>{
-  const {contenido}=props
+  const {children}=props
   return(
-    <Text>{contenido}</Text>
+    <Text>{children}</Text>
   )
 }
 
@@ -15,9 +15,9 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Texto contenido="Hola"></Texto>
-      <Texto contenido="Mundo"></Texto>
-      <Texto contenido="React Native"></Texto>
+      <Texto>Hola</Texto>
+      <Texto>Mundo</Texto>
+      <Texto>React Native</Texto>
       
       <Button title='Presioname!'></Button>
       <StatusBar style="auto" />
